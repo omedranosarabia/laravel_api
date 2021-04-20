29240ff0f24c1a1ac782f213e900704fed1ade13
Omar Medrano <omar.medrano.ms@gmail.com>
2021-04-20 17:50:25 -0500

    Versión 2: colección

--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return new PostColection(Post::latest()->paginate());
     }
 
     /**
--- app/Http/Resources/V2/PostColection.php
+++ app/Http/Resources/V2/PostColection.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PostColection extends ResourceCollection
 {
+    public $collects = PostResource::class;
+
     /**
      * Transform the resource collection into an array.
      *
@@ -14,6 +16,16 @@ class PostColection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'OmedranoSarabia',
+                    'Platzi',
+                ],
+            ],
+            'type' => 'articles'
+        ];
     }
 }

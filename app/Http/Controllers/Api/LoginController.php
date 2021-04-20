71d5d6e53adf3b8e63406e3c46caef6513bd32a1
Omar Medrano <omar.medrano.ms@gmail.com>
2021-04-20 18:09:17 -0500

    Autenticación

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        //login true
+        $this->validateLogin($request);
+
+        //login false
+    }
+    
+    public function validateLogin(Request $request)
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required', // Desde dónde se está conectando este usuario
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
+use Illuminate\Support\Facades\Http;
 
 // V1
 Route::apiResource('v1/posts', PostV1::class)
@@ -20,3 +21,6 @@ Route::apiResource('v2/posts', PostV2::class)
         'index',
         'show',
     ])->middleware('auth:sanctum');
+
+// Login
+Route::post('login', [\App\Http\Controllers\Api\LoginController::class, 'login']);

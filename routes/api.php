7413549d27a526094a774454bbfcd2dbff05f432
Omar Medrano <omar.medrano.ms@gmail.com>
2021-04-20 17:09:41 -0500

    Versión 2: planificación y configuración inicial

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)
-->only([
-    'index',
-    'show',
-    'destroy',
+// V1
+Route::apiResource('v1/posts', PostV1::class)
+    ->only([
+        'index',
+        'show',
+        'destroy',
+    ]);
+
+// V2
+Route::apiResource('v2/posts', PostV2::class)
+    ->only([
+        'index',
+        'show',
     ]);

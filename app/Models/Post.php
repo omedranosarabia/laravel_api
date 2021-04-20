1c5c8119eecca0e1c3bafe4d55c2e8a3d199fdd6
Omar Medrano <omar.medrano.ms@gmail.com>
2021-04-20 17:38:23 -0500

    Versión 2: recurso

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,9 +13,14 @@ class Post extends Model
     {
         return substr($this->content, 0, 120);
     }
-    
+
     public function getPublishedAtAttribute()
     {
         return $this->created_at->format('d/m/Y');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
